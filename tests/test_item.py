4605f908ed58ad8e14718c322bbc5ee9751a30fd
Polina <polinakiseleva4@gmail.com>
2023-03-25 14:14:06 +0200

    Создала атрибуты для экземпляра класса, реализовала методы calculate_total_price и apply_discount, написала тесты.

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item('Смартфон', 10000, 20)
+
+
+def test_item_init(item):
+    assert item.name == 'Смартфон'
+    assert item.price == 10000
+    assert item.quantity == 20
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+
+def test_apply_discount(item):
+    item.apply_discount()
+    assert item.price == 10000
