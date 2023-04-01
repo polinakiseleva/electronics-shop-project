cedac0d0bd232a277f7eb6abec983d4d1fe33933
Polina <polinakiseleva4@gmail.com>
2023-04-01 12:19:31 +0200

    Сделала приватным атрибут name у класса Item, добавила для name геттер и сеттер Реализовала методы instantiate_from_csv и string_to_number

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+
+CVS_FILE = '../src/items.csv'
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +18,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, product: str):
+        if len(product) > 10:
+            raise ValueError('Длина наименования товара превышает 10 символов.')
+        else:
+            self.__name = product
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +47,18 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+
+        with open(CVS_FILE, encoding='windows-1251') as file:
+            file_reader = csv.DictReader(file, delimiter=',')
+            for item in file_reader:
+                cls.all.append(item)
+
+    @staticmethod
+    def string_to_number(string: str) -> int:
+        try:
+            return int(string)
+        except ValueError:
+            return int(string[0: string.find('.')])
