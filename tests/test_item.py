89aebc6540ff327fe811b074bd87fbf496093090
Polina <polinakiseleva4@gmail.com>
2023-04-01 16:33:02 +0200

    Написала тесты для новых методов

--- tests/test_item.py
+++ tests/test_item.py
@@ -20,3 +20,17 @@ def test_calculate_total_price(item):
 def test_apply_discount(item):
     item.apply_discount()
     assert item.price == 10000
+
+
+def test_correct_name(item):
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+
+def test_incorrect_name(item):
+    with pytest.raises(ValueError):
+        item.name = 'СуперСмартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('8.4') == 8
